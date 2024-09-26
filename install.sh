102eacce64342aa8fad6864200408f438192e349
xcvzolda <166529366+xcvzolda@users.noreply.github.com>
2024-09-26 19:51:09 +0300

    Update 1.1.1.1

---
+++ install.sh
@@ -0,0 +1,7 @@
+pkg install nano
+pkg upgrade nano
+pkg install w3m
+pkg install otter-browser
+wget https://raw.githubusercontent.com/Yisus7u7/termux-desktop-xfce/main/boostrap.sh
+apt update > /dev/null 2>&1 && apt --assume-yes install wget > /dev/null 2>&1 && wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh -q
+bash InstallTools.sh
