ca7dcfa97e266be9b022cb5944e6fbb53731862a
xcvzolda <166529366+xcvzolda@users.noreply.github.com>
2024-09-27 13:26:28 +0300

    Unstable/stable version

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@ pkg install nano
 pkg upgrade nano
 pkg install w3m
 pkg install otter-browser
+wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh && bash InstallTools.sh
 wget https://raw.githubusercontent.com/Yisus7u7/termux-desktop-xfce/main/boostrap.sh
 
