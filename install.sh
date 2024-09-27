0f8acf4959b99825548a8c397fc3e5aa9b64c297
xcvzolda <166529366+xcvzolda@users.noreply.github.com>
2024-09-27 13:24:41 +0300

    Possible fix? 50% complete

--- install.sh
+++ install.sh
@@ -3,5 +3,4 @@ pkg upgrade nano
 pkg install w3m
 pkg install otter-browser
 wget https://raw.githubusercontent.com/Yisus7u7/termux-desktop-xfce/main/boostrap.sh
-apt update > /dev/null 2>&1 && apt --assume-yes install wget > /dev/null 2>&1 && wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh -q
-./InstallTools.sh
+
